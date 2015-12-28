1879b1c486dfe2bb51ca8c21fb33d2fe010f01f6
zhangwb <wenbing.zhang@nttdata.com>
2015-12-28 13:23:46 +0800

    jQuery的特效 完成

--- jQuery_introduction/js/animate.js
+++ jQuery_introduction/js/animate.js
@@ -159,6 +159,55 @@ $(document).ready(function () {
         myDiv.stop();
     });
 
+    // .finish()
+    var horiz = $("#path").width() - 20,
+        vert = $("#path").height() - 20;
+
+    var btns = {
+        bsst: function () {
+            $("div.box").stop(true, true);
+        },
+        bs: function () {
+            $("div.box").stop();
+        },
+        bsft: function () {
+            $("div.box").stop(false, true);
+        },
+        bf: function () {
+            $("div.box").finish();
+        },
+        bcf: function () {
+            $("div.box").clearQueue().finish();
+        },
+        bsff: function () {
+            $("div.box").stop(false, false);
+        },
+        bstf: function () {
+            $("div.box").stop(true, false);
+        },
+        bcs: function () {
+            $("div.box").clearQueue().stop();
+        }
+    };
+
+    $("button.b").on("click", function () {
+        btns[this.id]();
+    });
+
+    $("#go2").click(function () {
+        $(".box").clearQueue()
+            .stop()
+            .css({
+                left: 20,
+                top: 10
+            }).animate({
+            top: vert
+        }, 3000).animate({
+            left: horiz
+        }, 3000).animate({
+            top: 10
+        }, 3000);
+    });
 
     // 渐变
     // .fadeIn()  .fadeOut()  .fadeTo()  .fadeToggle()
